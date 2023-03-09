2579526791524e890c30e4593310ad064d379afe
Crxshxr <103609586+Crxshxr@users.noreply.github.com>
2023-03-09 00:25:12 -0500

    go

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
+import "./App.css";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = useState([
+		{
+			text: "React Hooks in Depth",
+			isCompleted: false
+		},
+		{
+			text: "Write Articles @ Medium",
+			isCompleted: false
+		},
+		{
+			text: "Share article at Reddit",
+			isCompleted: false
+		}
+	]);
+	
+	const addTodo = text => {
+		const newTodos = [...todos, { text }];
+		setTodos(newTodos);
+	};
+	
+	const completeTodo = index => {
+		const newTodos = [...todos];
+		newTodos[index].isCompleted = !newTodos[index].isCompleted;
+		setTodos(newTodos);
+	};
+	
+	const removeTodo = index => {
+		const newTodos = [...todos];
+		newTodos.splice(index, 1);
+		setTodos(newTodos);
+	};
+	
+	return (
+		<div className="app">
+			<div className="todo-list">
+				<h1 className="title">Create a Todo with React Hooks</h1>
+				{todos.map((todo, index) => (
+					<Todo
+						key={index}
+						index={index}
+						todo={todo}
+						completeTodo={completeTodo}
+						removeTodo={removeTodo}
+					/>
+				))}
+				<TodoForm addTodo={addTodo} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
